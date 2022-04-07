a8a1ac887cce6cc5ee230ef369af7afd27b33bab
aleganza <ganzarolli.alessio@gmail.com>
2022-04-07 22:21:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     // il tuo token
     $token = "5100404136:AAHEr_gwp5d-q6HXP3c9ls-2D_CC0H7WI_w";
-    $url = "https://api.telegram.org/bot".$token."/getMe";
+    $url = "https://api.telegram.org/bot".$token."/getUpdates";
 
     $request = curl_init($url);
 
