37713105d50829a3068e9266a6ed17e9b23624d4
aleganza <ganzarolli.alessio@gmail.com>
2022-04-08 23:45:08 +0200

    send message
    simple function for sending messages

--- config.php
+++ config.php
@@ -1,7 +1,6 @@
 <?php
     function fetchApi($url){
         $req = curl_init($url);
-
         $resp = curl_exec($req);
 
         if($resp == false){
@@ -16,18 +15,44 @@
     }
 
     class Telegram{
-        protected $token;
+        protected $token; // token bot
+        protected $tUrl; // url di telegram api
 
         function __construct($token){
-            $this->token = $token;
+            /* $this->token = $token; */
+            $this->tUrl = "https://api.telegram.org/bot".$token; // url a cui fare le richieste
         }
-        function setUrl($methodName){
-            $url = "https://api.telegram.org/bot".$this->token."/".$methodName;
+        function setUrl($method){
+            $url = $this->tUrl."/".$method;
             return $url;
         }
         function getMe(){
             $url = $this->setUrl("getMe");
             fetchApi($url);
+        }
+        function getUpdates(){
+            $url = $this->setUrl("getUpdates");
+            fetchApi($url);
+        }
+        function getDataJson($method){
+            $url = $this->setUrl($method);
+
+            $json = file_get_contents($url);
+            file_put_contents("data.log", $json."\n", FILE_APPEND);
+
+            return json_decode($json, true); // ritorno il json come variabile php
+        }
+        function sendMessage($chatId){
+            // dati per inviare il messaggio
+            $data = [
+                'chat_id' => $chatId, 
+                'text' => "funziona"
+            ];
+
+            // richiesta per inviare il messaggio
+            $url = $this->setUrl("sendMessage?".http_build_query($data));
+
+            /* $response = file_get_contents($url); */
         }
     }
 ?>
--- index.php
+++ index.php
@@ -5,7 +5,14 @@
         $token = "5100404136:AAHEr_gwp5d-q6HXP3c9ls-2D_CC0H7WI_w"; //token
         $bot = new Telegram($token);
     
-        $bot->getMe();
+        /* $bot->getMe(); */
+        $bot->getUpdates();
+
+        $dataJson = $bot->getDataJson("getUpdates");
+        $idChat = $dataJson["result"][0]["message"]["chat"]["id"];  // DA CHIEDERE AL GCAN SE SI PUO FARE MENO INCASINATO
+
+        $bot->sendMessage($idChat);
+
     }catch(ErrorException $e){
         echo $e->getMessage();
     }
